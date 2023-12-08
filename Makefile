e9768212c9f61e0d0fe25068de85e191876a44b7
LukasCsader <csader.lukas@gmail.com>
2023-12-08 18:56:47 +0100

    radiator heating the room

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 build: 
 	cd src && make build
 	cp src/simulation simulation
-
+clean:
+	rm -rf simulation
+	cd src && make clean
 run:
 	./simulation
--- src/main.cpp
+++ src/main.cpp
@@ -10,41 +10,47 @@ private:
     double OFF_temperature;
     Variable target_temperature;
 public:
+    Input temperature_sensor;
     Variable output;
-    Thermostat(double on, double off, Integrator temperature_sensor):
+    Thermostat(double on, double off, Input temperature_sensor):
         ON_temperature(on),
         OFF_temperature(off),
         target_temperature(on),
-        output(0),
+        temperature_sensor(temperature_sensor),
+        output(1),
         Condition(target_temperature - temperature_sensor)
         {};
     void Action(){
-        if(Up()){
+    cout << "Entering Thermostat::Action()" << endl;
+
+     if(Up()){
             output = 1.0;
             target_temperature = OFF_temperature;
         }else{
             output = 0.0;
-            target_temperature = OFF_temperature;
+            target_temperature = ON_temperature;
         }
-        printf("Thermostat: %-1.1f", output.Value());
+        printf("Thermostat: %-1.1f", output.Value());// ... rest of the code ...
+
+    cout << "Exiting Thermostat::Action()" << endl;
     }
 };
 
-const double MassFlowRate = 0.01;           // in kg/s
+const double Mass = 20;           // in kg
 const double SpecificHeatCapacity = 4186.0; // in J/(kg·°C) water
-const double HeatLossCoefficient = 0.1;
+const double HeatLossCoefficient = 0.004;
 
 class Radiator : public aContiBlock {
     private:
     public:
     Integrator temperature;
+    Input heaterPower;
+    Input outsideTemperature;
 
-    Radiator(Input heaterPower, Input outsideTemperature, double massFlowRate, double specificHeatCapacity, double heatLossCoefficient) :
-        temperature((heaterPower - (heatLossCoefficient * (temperature - outsideTemperature))) / (massFlowRate * specificHeatCapacity)) {}
+    Radiator(Input heaterPower, Input outsideTemperature, double Mass, double specificHeatCapacity, double heatLossCoefficient) :
+    heaterPower(heaterPower), outsideTemperature(outsideTemperature),
+    temperature((heaterPower /Mass * specificHeatCapacity) - ((heatLossCoefficient * (temperature - outsideTemperature)) / Mass * specificHeatCapacity)) {}
 
-    void Out(){
-        printf("Radiator temperature: %-1.1f", temperature.Value());
-    }
      double Value() override {
     return temperature.Value();
 }
@@ -53,27 +59,20 @@ class Radiator : public aContiBlock {
 
 class cell {
 private:
-    Radiator *radiator;
     bool shouldConsumePower;
 
 public:
+    Radiator *radiator;
     Integrator temperature;
+    Input temp;
 
-    cell(Radiator* Radiator, bool consumePower, Input temp) : temperature(temp, 0.), radiator(Radiator), shouldConsumePower(consumePower) {}
-
-    void ConsumeRadiatorPower() {
-        if (shouldConsumePower) {
-            Input powerEquation = 3. * radiator->Value();  
-            temperature.SetInput(powerEquation);
-        }
-        std::cout << "ConsumeRadiatorPower called" << std::endl;
-
+    cell(Radiator* Radiator, bool consumePower, Input temp) : temp(temp), temperature(temp,0.), radiator(Radiator), shouldConsumePower(consumePower) {
     }
 
     void Out(){
         printf("%-1.1f", temperature.Value());
         if (shouldConsumePower){
-            printf(" Radiator: %-1.1f", radiator->Value());
+            printf("<-Radiator");
         }
     }
 
@@ -87,7 +86,6 @@ public:
 
 // needs to be stored on heap because it doesn't work if its just array
 cell *room[X_SIZE][Y_SIZE][Z_SIZE];
-
 
 void Sample(){
     printf("######################## %-9.3f ########################\n", T.Value());
@@ -115,16 +113,16 @@ void Sample(){
 }
 
 Sampler S(Sample,10);
-#define heat_transfer_equation(T1, T2) (-0.1 * ((T1) - (T2)))
+#define heat_transfer_equation(T1, T2) (-0.5 * ((T1) - (T2)))
 
 int main(int argc, char const *argv[])
-{
-    
-    Radiator radiator(20, 21.0, MassFlowRate, SpecificHeatCapacity, HeatLossCoefficient);
+{  
+    Radiator radiator(0., 0., Mass, SpecificHeatCapacity, HeatLossCoefficient);
+    //Thermostat thermostat(22.0, 18.0, room[0][7][0]->temperature);
     for (size_t x = 0, y = 0, z = 0;;)
     {
         bool consumePower = (x == 15 && y == 6 && z == 0);
-        room[x][y][z] = new cell(&radiator, consumePower,0.1);
+        room[x][y][z] = new cell(&radiator, consumePower,0.);
 
         if (++x >= X_SIZE)
             x = 0;
@@ -136,11 +134,11 @@ int main(int argc, char const *argv[])
             break;
     }
 
-
     for (size_t x = 0, y = 0, z = 0;;)
     {
         cell *current = room[x][y][z];
         Input equation = 0.0 * current->temperature;
+        
         if(x != 0)
             equation = equation + heat_transfer_equation(current->temperature, room[x-1][y][z]->temperature);
         if(x != X_SIZE - 1)
@@ -150,12 +148,22 @@ int main(int argc, char const *argv[])
         if(y != Y_SIZE - 1)
             equation = equation + heat_transfer_equation(current->temperature, room[x][y+1][z]->temperature);
 
-        current->temperature.SetInput(equation);
+        if (x == 15 && y == 6 && z == 0) {
+            current->radiator->heaterPower.Set(1500);
+            current->radiator->outsideTemperature.Set(equation);
+            current->radiator->temperature.Init(21.0);
 
-        current->temperature.Init(21.0 + double(x) / X_SIZE - double(y) / Y_SIZE + double(z) / Z_SIZE);
+            current->temperature.SetInput(0.0042 * current->radiator->temperature.Value());
+            current->temperature.Init(current->radiator->temperature.Value());
+        } else {
+            current->temperature.SetInput(equation);
+            current->temperature.Init(21.0 + double(x) / X_SIZE - double(y) / Y_SIZE + double(z) / Z_SIZE);
+        }
+        /*
+        if (x == 0 && y == 7 && z == 0) 
+            thermostat.temperature_sensor.Set(current->temperature.Value());
+        */
         
-        current->ConsumeRadiatorPower();
-
         current->Out();
 
         if(x == X_SIZE - 1)
@@ -172,10 +180,9 @@ int main(int argc, char const *argv[])
         if (++z >= Z_SIZE)
             break;
     }
-
-    Thermostat(1.0, 0.0, room[0][7][0]->temperature);
-
-    Init(0);
+   
+    //DebugON();
+    Init(0.,500);
     SetStep(1e-10,0.5);
     Run();
     return 0;
